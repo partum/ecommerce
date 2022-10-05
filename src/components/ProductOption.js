6d62a10f57f92adbc3dbf4b2123fdebf0a06e103
Julia Arrington <jarringt@uccs.edu>
2022-10-04 18:51:43 -0600

    product page skeleton

--- src/components/ProductOption.js
+++ src/components/ProductOption.js
@@ -4,10 +4,27 @@ export default function ProductOption(){
     console.log("wtf")
     return(
     <div className="productOption">
-        <div className="productOption__header">
+        <div className="productOption__pre">
+            <div>new</div>
             <span>Gender</span>
             <span>rating</span>
         </div>
+        <div className="productOption__header">
+            <h1>PRODUCT TITLE</h1>
+            <span>heart</span>
+        </div>
+        <div className="productOption__price">
+            <div>price</div>
+            <div>afterpay</div>
+            <div>special offer</div>
+        </div>
+        <div className="productOption__options">
+            maybe this should be it's own thing???
+        </div>
+        <div className="productOption__size">
+            sizes, also chart link
+        </div>
+        <button>Add To Cart</button>
     </div>
     )
 }
